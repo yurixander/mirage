10e7929360026104b88251a7e67ac06bb3321d9d
Tokyoto <88kirisu@gmail.com>
2023-11-11 03:58:14 +0100

    Removed unused icon libraries, #5

---
+++ src/stories/channel.stories.tsx
@@ -0,0 +1,18 @@
+import {Meta, StoryObj} from "@storybook/react"
+import Channel, {ChannelProps} from "../components/Channel"
+
+type Story = StoryObj<typeof Channel>
+
+const meta: Meta<typeof Channel> = {component: Channel}
+
+export const Default: Story = {
+  render: (args: ChannelProps) => <Channel {...args} />,
+  args: {
+    name: "Help & guides",
+    icon: "string",
+    isActive: true ,
+    notification: 0,
+  }
+}
+
+export default meta
