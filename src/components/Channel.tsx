e1c42acb0b84ba25be313a438a38b3a7da3369b5
yurixander <101931215+yurixander@users.noreply.github.com>
2023-11-11 14:56:11 -0500

    Fix missing line break, remove extra spaces

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,7 +1,7 @@
 import "../styles/Channel.sass"
-import { ReactComponent as IconHash } from "../../public/icons/hash.svg";
-import NotificationDot from "./NotificationDot";
-import { assert, cut, cutName } from "../util";
+import {ReactComponent as IconHash} from "../../public/icons/hash.svg"
+import NotificationDot from "./NotificationDot"
+import {assert, cut} from "../util"
 
 export type ChannelProps = {
   name: string
@@ -11,18 +11,19 @@ export type ChannelProps = {
   mentionCount: number
 }
 
-export default function Channel(props: ChannelProps){
+export default function Channel(props: ChannelProps) {
   assert(props.name.length !== 0, "channel name should not be empty")
 
   // Determine CSS class to apply based on the active state of the channel.
   const classNameActive = props.isActive ? "active" : ""
+
   const mentionCountProp = props.mentionCount > 0 ? props.mentionCount : undefined
 
   return (
     <div className="Channel">
-      <IconHash className={"IconHash " + classNameActive}/>
+      <IconHash className={"IconHash " + classNameActive} />
       <span className={classNameActive}>{cut(props.name, 16)}</span>
-      {props.containsUnreadMessages && <NotificationDot mentionAmount={mentionCountProp}/>}
+      {props.containsUnreadMessages && <NotificationDot mentionAmount={mentionCountProp} />}
     </div>
   )
 }
