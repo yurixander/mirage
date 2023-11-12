95269e2e7d3c26d862a25c996ac4588f871b0001
yurixander <101931215+yurixander@users.noreply.github.com>
2023-11-11 22:33:04 -0500

    Add global hotkey for search bar

---
+++ src/hooks/useGlobalHotkey.ts
@@ -0,0 +1,32 @@
+import {useEffect} from "react"
+
+type KeyCombination = {
+  ctrl?: boolean
+  shift?: boolean
+  alt?: boolean
+  key: string
+}
+
+const useGlobalHotkey = (keyCombination: KeyCombination, callback: () => void) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const {ctrl, shift, alt, key} = keyCombination
+
+      if (
+        (event.ctrlKey === !!ctrl) &&
+        (event.shiftKey === !!shift) &&
+        (event.altKey === !!alt) &&
+        (event.key.toLowerCase() === key.toLowerCase())
+      ) {
+        event.preventDefault()
+        callback()
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+
+    return () => window.removeEventListener("keydown", handleKeyDown)
+  }, [keyCombination, callback])
+}
+
+export default useGlobalHotkey
