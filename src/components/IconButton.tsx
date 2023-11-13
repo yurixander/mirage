23ff5739e9716ad2bd1e5062593b77377168a276
yurixander <101931215+yurixander@users.noreply.github.com>
2023-11-13 12:45:10 -0500

    Use tooltip animation

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,6 +2,7 @@ import {Placement} from "tippy.js"
 import "../styles/IconButton.sass"
 import Tippy from "@tippyjs/react"
 import "tippy.js/dist/tippy.css"
+import "tippy.js/animations/scale-subtle.css"
 
 export type IconButtonProps = {
   onClick: () => void
@@ -11,14 +12,14 @@ export type IconButtonProps = {
 }
 
 export default function IconButton(props: IconButtonProps) {
-  const TOOLTIP_SHOW_DELAY = 1000
-
   // TODO: Style tooltip to have consistent typography (ex. default font size and weight, etc.).
   return (
     <Tippy
       content={props.tooltip}
       arrow={true}
-      delay={TOOLTIP_SHOW_DELAY}
+      inertia={true}
+      animation="scale-subtle"
+      duration={100}
       placement={props.tooltipPlacement}>
       <div
         className="IconButton"
