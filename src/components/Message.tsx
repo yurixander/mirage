7ab19bad6e1e476b00ec9f6783b66de690d45383
yurixander <101931215+yurixander@users.noreply.github.com>
2023-11-14 11:05:17 -0500

    Create basic implementation of `Message` component

---
+++ src/components/Message.tsx
@@ -0,0 +1,37 @@
+import "../styles/Message.sass"
+
+export type MessageProps = {
+  authorDisplayName: string
+  authorDisplayNameColor: string
+  authorAvatarUrl: string
+  text: string
+  timestamp: number
+  onAuthorClick: () => void
+}
+
+export default function Message(props: MessageProps) {
+  const time = new Date(props.timestamp)
+  const localeTimeString = time.toLocaleTimeString()
+
+  return (
+    <div className="Message">
+      <div className="wrapper">
+        <div className="avatar" onClick={() => props.onAuthorClick()}>
+          <img src={props.authorAvatarUrl} />
+        </div>
+        <div className="content">
+          <span
+            className="author-name"
+            style={{color: props.authorDisplayNameColor}}
+            onClick={() => props.onAuthorClick()}>
+            {props.authorDisplayName}
+          </span>
+          <div className="text">{props.text}</div>
+        </div>
+        <div className="time">
+          {localeTimeString}
+        </div>
+      </div>
+    </div>
+  )
+}
