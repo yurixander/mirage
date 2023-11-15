193b8bcc7504f887014830ee2ff08d375769994c
yurixander <101931215+yurixander@users.noreply.github.com>
2023-11-14 22:34:11 -0500

    Create `GuideArrow` and `TypingIndicator` components

---
+++ src/stories/guideArrow.stories.tsx
@@ -0,0 +1,33 @@
+import {Meta, StoryObj} from "@storybook/react"
+import GuideArrow, {GuideArrowOrientation, GuideArrowProps} from "../components/GuideArrow"
+
+type Story = StoryObj<typeof GuideArrow>
+
+const meta: Meta<typeof GuideArrow> = {component: GuideArrow}
+const render = (args: GuideArrowProps) => <GuideArrow {...args} />
+
+export const LeftOrientation: Story = {
+  render,
+  args: {
+    text: "This is some magical text!",
+    orientation: GuideArrowOrientation.Left
+  }
+}
+
+export const RightOrientation: Story = {
+  render,
+  args: {
+    text: "This is some magical text!",
+    orientation: GuideArrowOrientation.Right
+  }
+}
+
+export const LongText: Story = {
+  render,
+  args: {
+    text: "This is some magical text! It's also a long text. Very long text.",
+    orientation: GuideArrowOrientation.Right
+  }
+}
+
+export default meta
