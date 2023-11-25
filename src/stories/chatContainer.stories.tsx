706559e271b7472ae30b3177a9a2aa647474be7c
criss8X <carralerocristianx@gmail.com>
2023-11-25 11:21:19 -0500

    Implement chats section for `ChatContainer`

--- src/stories/chatContainer.stories.tsx
+++ src/stories/chatContainer.stories.tsx
@@ -1,18 +1,47 @@
 import {Meta, StoryObj} from "@storybook/react"
 import ChatContainer, {ChatContainerProps} from "../components/ChatContainer"
 import {RoomType} from "../components/Room"
+import Message from "../components/Message"
+import UnreadIndicator from "../components/UnreadIndicator"
+import EventMessage from "../components/EventMessage"
 
 type Story = StoryObj<typeof ChatContainer>
 
 const meta: Meta<typeof ChatContainer> = {component: ChatContainer}
 const render = (args: ChatContainerProps) => <ChatContainer {...args} />
 
+const userDisplayNameColor = "#5CC679"
+
 export const Default: Story = {
   render,
   args: {
     name: "Help & guides",
     text: "— Request assistance from your colleagues.",
-    type: RoomType.Text
+    type: RoomType.Text,
+    chatComponents: [
+      <Message
+        authorDisplayName={"John Doe"}
+        authorDisplayNameColor={"rgb(100, 200, 100)"}
+        authorAvatarUrl={"https://bestprofilepictures.com/wp-content/uploads/2020/07/Awesome-Profile-Picture-For-Facebook.jpg"} text={"The database hiccup turned into a full-on dance. We need to streamline our queries and possibly rethink our indexing strategy. Anyone up for a deep dive into our SQL?"}
+        timestamp={0}
+        onAuthorClick={() => { }} />,
+      <UnreadIndicator />,
+      <Message
+        authorDisplayName={"John Doe"}
+        authorDisplayNameColor={"rgb(100, 200, 100)"}
+        authorAvatarUrl={"https://bestprofilepictures.com/wp-content/uploads/2020/07/Awesome-Profile-Picture-For-Facebook.jpg"} text={"The database hiccup turned into a full-on dance. We need to streamline our queries and possibly rethink our indexing strategy. Anyone up for a deep dive into our SQL?"}
+        timestamp={0}
+        onAuthorClick={() => { }} />,
+      <Message
+        authorDisplayName={"John Doe"}
+        authorDisplayNameColor={"rgb(100, 200, 100)"}
+        authorAvatarUrl={"https://bestprofilepictures.com/wp-content/uploads/2020/07/Awesome-Profile-Picture-For-Facebook.jpg"} text={"The database hiccup turned into a full-on dance. We need to streamline our queries and possibly rethink our indexing strategy. Anyone up for a deep dive into our SQL?"}
+        timestamp={0}
+        onAuthorClick={() => { }} />,
+      <EventMessage content={<span>
+        <strong style={{color: userDisplayNameColor}}>Sapphire Pineapple</strong> changed their name to <strong style={{color: userDisplayNameColor}}>Snappy Turtle</strong>.
+      </span>} timestamp={Date.now()} />
+    ]
   }
 }
 
