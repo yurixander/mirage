46a6732f754252cc51e8bcf96d8e625eeaee805e
Tokyoto <88kirisu@gmail.com>
2023-12-04 22:03:13 -0500

    Now  ´Message´ component is global

---
+++ src/components/MessageText.tsx
@@ -0,0 +1,43 @@
+import "../styles/MessageText.sass"
+import {timeFormatter} from "../util"
+import ContextMenu from "./ContextMenu"
+import {faReply} from '@fortawesome/free-solid-svg-icons'
+import Message from "./Message"
+
+
+export type MessageTextProps = {
+  authorDisplayName: string
+  authorDisplayNameColor: string
+  authorAvatarUrl: string
+  text: string
+  timestamp: number
+  onAuthorClick: () => void
+}
+
+export default function MessageText(props: MessageTextProps) {
+  return (
+    <ContextMenu items={[
+      {
+        label: "Reply",
+        action: () => { },
+        icon: faReply
+      },
+      {
+        label: "Resend",
+        action: () => { }
+      },
+      {
+        label: "Pin",
+        action: () => { }
+      }
+    ]} children={
+      <Message
+        authorDisplayName={props.authorDisplayName}
+        authorDisplayNameColor={props.authorDisplayNameColor}
+        authorAvatarUrl={props.authorAvatarUrl}
+        content={<div className="message-text">{props.text}</div>}
+        timestamp={props.timestamp}
+        onAuthorClick={props.onAuthorClick} />
+    } />
+  )
+}
--- src/stories/textMessage.stories.tsx
+++ src/stories/textMessage.stories.tsx
@@ -1,18 +1,18 @@
 import {Meta, StoryObj} from "@storybook/react"
-import Message, {MessageProps} from "../components/Message"
+import MessageText, {MessageTextProps} from "../components/MessageText"
 
-type Story = StoryObj<typeof Message>
+type Story = StoryObj<typeof MessageText>
 
-const meta: Meta<typeof Message> = {component: Message}
-const render = (args: MessageProps) => <Message {...args} />
+const meta: Meta<typeof MessageText> = {component: MessageText}
+const render = (args: MessageTextProps) => <MessageText {...args} />
 
 export const Default: Story = {
   render,
   args: {
     authorDisplayName: "John Doe",
-    // TODO: Demo avatar URL.
     authorAvatarUrl: "https://bestprofilepictures.com/wp-content/uploads/2020/07/Awesome-Profile-Picture-For-Facebook.jpg",
     authorDisplayNameColor: "rgb(100, 200, 100)",
+    text: "Hello world!",
     timestamp: Date.now(),
     onAuthorClick: () => { },
   }
