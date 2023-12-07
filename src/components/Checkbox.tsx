a3d432a90f68c36be8b0a1d1646da590b07a4601
yurixander <101931215+yurixander@users.noreply.github.com>
2023-12-07 15:06:41 -0500

    Improve `Checkbox` component: add label prop and styling

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,16 +1,27 @@
+import {useState} from "react"
 import "../styles/Checkbox.sass"
 
-export type CheckBoxProps = {
-  isSelected: boolean,
-  onClick: () => void
+export type CheckboxProps = {
+  isInitiallySelected: boolean,
+  onSelectionChange: (isSelected: boolean) => void
+  label?: string
 }
 
-export default function ToggleButton(props: CheckBoxProps) {
-  const isSelectedClassName = props.isSelected ? "selected" : "default"
+export default function Checkbox(props: CheckboxProps) {
+  const [isSelected, setSelected] = useState(props.isInitiallySelected)
+  const isSelectedClassName = isSelected ? "selected" : "default"
+
+  const handleSelectionChanged = () => {
+    const isNowSelected = !isSelected
+
+    setSelected(isNowSelected)
+    props.onSelectionChange(isNowSelected)
+  }
 
   return (
-    <div className="ToggleButton">
-      <div className={"container " + isSelectedClassName} onClick={props.onClick} />
+    <div className="Checkbox">
+      <div className={"container " + isSelectedClassName} onClick={handleSelectionChanged} />
+      {props.label && <div className="label">{props.label}</div>}
     </div>
   )
 }
