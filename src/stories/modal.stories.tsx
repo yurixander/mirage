bdbed6c154745c7c95f1e329af6f2ed6596f9f1f
Tokyoto <88kirisu@gmail.com>
2023-12-09 11:03:43 -0500

    Move ´WelcomePopup´ component to `Modal` stories.

--- src/stories/modal.stories.tsx
+++ src/stories/modal.stories.tsx
@@ -1,6 +1,9 @@
 import {Meta, StoryObj} from "@storybook/react"
 import Modal, {ModalProps} from "../components/Modal"
-import Button from "../components/Button"
+import Button, {ButtonStyle} from "../components/Button"
+import {ReactComponent as Stars} from "../../public/icons/stars.svg"
+import {ReactComponent as DotGrid} from "../../public/icons/dot-grid.svg"
+import "../styles/WelcomePopup.sass"
 
 type Story = StoryObj<typeof Modal>
 
@@ -17,6 +20,38 @@ export const WithTwoDialogs: Story = {
       <div className="TestPopup">
         <Button onClick={() => { }} text={"showPopup2"} />
         <Button onClick={() => { }} text={"close"} />
+      </div>
+    ]
+  }
+}
+
+export const WithWelcomePopup: Story = {
+  render,
+  args: {
+    dialogs: [
+      <div className="WelcomePopup">
+        <div className="header">
+          <h3>Hey you!</h3>
+          <h1>You’re beautiful.</h1>
+          <Stars className="stars" />
+        </div>
+        <div className="body">
+          <span className="text">
+            <b>Did you know that?</b> You’re a beautiful person, with a charming personality. My grandmother always used to say—”Cows are beautiful cloven-hooved herbivores, and so should you!”
+          </span>
+          <span>
+            I hope that resonates with you as much as it did with me.
+          </span>
+          <div className="dot-grid">
+            <DotGrid />
+          </div>
+        </div>
+        <div className="actions">
+          <Button
+            text="Got it, thanks ⟶"
+            style={ButtonStyle.Default}
+            onClick={() => {/* TODO: Handle click on View messages button. */}} />
+        </div>
       </div>
     ]
   }
