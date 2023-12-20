84c5e9897bfbc9fd8ef910cdbffeb9af93966ae8
criss8X <carralerocristianx@gmail.com>
2023-12-20 14:31:32 -0500

    Fixed ´svgDeclaration´ module and upgrade storybook

--- src/types/svgDeclaration.ts
+++ src/types/svgDeclaration.ts
@@ -1,12 +1,6 @@
-import "react"
-
-declare module "*.svg" {
-  import React = require("react")
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
-  >
-  const src: string
-  export default src
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
-
-export { }
