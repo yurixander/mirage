a17d7b4e4e408572c755a50a6cb31402ac2242b0
criss8X <carralerocristianx@gmail.com>
2024-01-26 20:42:30 -0500

    refactor: Migrate two medium components to TailwindCSS

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,15 +1,24 @@
 import {type FC} from "react"
-import "../styles/Loader.sass"
+import {twMerge} from "tailwind-merge"
 
 export type LoaderProps = {
   text?: string
 }
 
 const Loader: FC<LoaderProps> = ({text}) => {
+  const loaderContainerTwClassName = "flex justify-center items-center"
+  const loaderTwClassName = twMerge(
+    "h-loaderSize w-loaderSize",
+    " rounded-50 border-3 border-solid border-white",
+    "border-t-transparent border-b-transparent",
+    "text-center self-center origin-center",
+    "transition-opacity duration-300 ease-in animate-spin"
+  )
+
   return (
-    <div className="Loader">
-      <div className="loader"></div>
-      {text && <span className="text">{text}</span>}
+    <div className={loaderContainerTwClassName}>
+      <div className={loaderTwClassName}></div>
+      {text && <span className="ml-1ch">{text}</span>}
     </div>
   )
 }
