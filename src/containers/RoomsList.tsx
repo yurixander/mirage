3a428277f8a5ffa375fd5fa2b377aa297fd37dc9
criss8X <carralerocristianx@gmail.com>
2024-02-08 11:13:23 -0500

    chore: Update ESLint config to use new plugin

--- src/containers/RoomsList.tsx
+++ src/containers/RoomsList.tsx
@@ -2,17 +2,12 @@ import {useMemo, type FC} from "react"
 import Label from "../components/Label"
 import useRooms from "@/hooks/matrix/useRooms"
 import Room, {RoomType} from "@/components/Room"
+import {NotificationCountType} from "matrix-js-sdk"
 
 const RoomsList: FC = () => {
   const {rooms} = useRooms()
 
   // TODO: Continue implementation.
-
-  // const handleAddRoom: MatrixEventCallback<string | Room> = () => {
-  //   return fetchRooms
-  // }
-
-  // const handleRemoveRoom = () => {}
 
   // const rooms = useSyncedMap<string, Room>(
   //   ClientEvent.Room,
@@ -21,72 +16,61 @@ const RoomsList: FC = () => {
   //   handleRemoveRoom
   // )
 
-  // const textChannels = useMemo(
-  //   () => rooms.filter(room => room.getType === RoomType.Text),
-  //   [rooms]
-  // )
-
-  // const roomElements = useMemo(
-  //   () =>
-  //     textChannels.map((channel, index) => (
-  //       <Room
-  //         key={index}
-  //         name={channel.name}
-  //         type={channel.type}
-  //         isActive={channel.isActive}
-  //         containsUnreadMessages={channel.containsUnreadMessages}
-  //         mentionCount={channel.mentionCount}
-  //         onClick={channel.onClick}
-  //       />
-  //     )),
-  //   [textChannels]
-  // )
+  const spaceElements = useMemo(
+    () =>
+      rooms
+        ?.filter(room => room.isSpaceRoom())
+        .map((room, index) => (
+          <Room
+            key={index}
+            name={room.name}
+            type={RoomType.Space}
+            isActive={false}
+            containsUnreadMessages={
+              room.getUnreadNotificationCount(NotificationCountType.Total) > 0
+            }
+            mentionCount={room.getUnreadNotificationCount(
+              NotificationCountType.Highlight
+            )}
+            onClick={() => {}}
+          />
+        )),
+    [rooms]
+  )
 
   const roomElements = useMemo(
     () =>
-      rooms?.map((room, index) => (
-        <Room
-          key={index}
-          name={room.name}
-          type={RoomType.Text}
-          isActive={false}
-          containsUnreadMessages={false}
-          mentionCount={0}
-          onClick={function (): void {
-            throw new Error("Function not implemented.")
-          }}
-        />
-      )),
+      rooms
+        ?.filter(room => !room.isSpaceRoom())
+        .map((room, index) => (
+          <Room
+            key={index}
+            name={room.name}
+            type={RoomType.Text}
+            isActive={false}
+            containsUnreadMessages={
+              room.getUnreadNotificationCount(NotificationCountType.Total) > 0
+            }
+            mentionCount={room.getUnreadNotificationCount(
+              NotificationCountType.Highlight
+            )}
+            onClick={() => {}}
+          />
+        )),
     [rooms]
   )
-  // const spaceElements = useMemo(
-  //   () =>
-  //     spaces.map((space, index) => (
-  //       <Room
-  //         key={index}
-  //         name={space.name}
-  //         type={space.type}
-  //         isActive={space.isActive}
-  //         containsUnreadMessages={space.containsUnreadMessages}
-  //         mentionCount={space.mentionCount}
-  //         onClick={space.onClick}
-  //       />
-  //     )),
-  //   [spaces]
-  // )
 
   return (
     <section className="flex h-full flex-col gap-8 p-1 scrollbar-hide">
       <nav className="flex flex-col gap-4">
         <Label text="Spaces" />
 
-        {/* {spaceElements} */}
+        {spaceElements}
       </nav>
 
       <nav className="flex flex-col gap-4">
-        <Label text="Channels" />
+        <Label text="Rooms" />
         {roomElements}
-        {/* {roomElements} */}
       </nav>
     </section>
   )
