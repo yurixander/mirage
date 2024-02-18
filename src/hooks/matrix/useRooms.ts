26de5944a09c9554ddf5cedd18f8a73916252eea
yurixander <101931215+yurixander@users.noreply.github.com>
2024-02-18 15:25:49 -0500

    refactor: Proper types for `useEventListener`

--- src/hooks/matrix/useRooms.ts
+++ src/hooks/matrix/useRooms.ts
@@ -1,6 +1,5 @@
 import useConnection from "@/hooks/matrix/useConnection"
-import useEventListener from "@/hooks/matrix/useEventListener"
-import {ClientEvent, type MatrixClient, type Room} from "matrix-js-sdk"
+import {type Room} from "matrix-js-sdk"
 import {useEffect, useState} from "react"
 import useActiveRoom from "./useActiveRoom"
 
@@ -22,13 +21,6 @@ const useRooms = () => {
 
     setRooms(client.getRooms())
   }, [client, syncState])
-
-  // REVIEW: Should this be inside a `useEffect`? Or does it automatically handle cleanup (ie. removing the event listener) when this hook is unmounted?
-  // Listen for room updates, and update the state accordingly.
-  useEventListener(ClientEvent.Room, (event, state, previousStateEvent) => {
-    // TODO: Add or remove rooms from the state based on the event.
-    console.log("Room event:", event, state, previousStateEvent)
-  })
 
   return {rooms, activeRoomId, setActiveRoomId}
 }
