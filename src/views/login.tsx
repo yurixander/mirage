57b7ae6b7bf2d5bc97bd025f8c85eac5f213eb5a
criss8X <carralerocristianx@gmail.com>
2024-02-25 11:50:15 -0500

    fix: Resolve infinite loop issue on authentication

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -10,6 +10,7 @@ import {
   faEye,
   faLink,
   faUserCircle,
+  faKey,
 } from "@fortawesome/free-solid-svg-icons"
 import Button, {ButtonColor, ButtonVariant} from "@/components/Button"
 import useCachedCredentials from "@/hooks/matrix/useCachedCredentials"
@@ -17,20 +18,18 @@ import useConnection from "@/hooks/matrix/useConnection"
 import {ViewPath} from "@/utils/util"
 import {Link, useNavigate} from "react-router-dom"
 import AppLogo from "@/components/AppLogo"
+import {SyncState} from "matrix-js-sdk"
 
 const LoginView: FC = () => {
   const navigate = useNavigate()
   const {credentials, saveCredentials} = useCachedCredentials()
-  const [userId, setUserId] = useState("@thecriss:matrix.org")
+  const [userId, setUserId] = useState("")
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-  const [serverUrl, setServerUrl] = useState("https://matrix-client.matrix.org")
+  const [baseUrl, setBaseUrl] = useState("")
+  const [accessToken, setAccessToken] = useState("")
 
   const {connect, disconnect, syncState, lastSyncError, isConnecting} =
     useConnection()
-
-  const [accessToken, setAccessToken] = useState(
-    "syt_dGhlY3Jpc3M_yBPDvDMuDeMKQFonYlQM_09tZYX"
-  )
 
   const login = useCallback(async () => {
     if (isConnecting) {
@@ -39,7 +38,7 @@ const LoginView: FC = () => {
 
     const connectedAndSynced = await connect({
       accessToken,
-      baseUrl: serverUrl,
+      baseUrl,
       userId,
     })
 
@@ -47,7 +46,7 @@ const LoginView: FC = () => {
       return
     }
 
-    saveCredentials({accessToken, baseUrl: serverUrl, userId})
+    saveCredentials({accessToken, baseUrl, userId})
     await disconnect()
     navigate(ViewPath.App)
   }, [
@@ -57,117 +56,132 @@ const LoginView: FC = () => {
     isConnecting,
     navigate,
     saveCredentials,
-    serverUrl,
+    baseUrl,
     userId,
   ])
 
   // Automatically login if credentials are cached.
   useEffect(() => {
-    if (credentials === null) {
+    if (credentials === null || isConnecting || syncState === SyncState.Error) {
       return
     }
 
-    void login()
-  }, [credentials, login])
+    void connect({...credentials}).then(async connectedAndSynced => {
+      if (!connectedAndSynced) {
+        return
+      }
+
+      await disconnect()
+      navigate(ViewPath.App)
+    })
+  }, [connect, credentials, disconnect, isConnecting, navigate, syncState])
 
   return (
-    <div className="flex size-full items-center justify-center">
-      <div className="flex size-96 flex-col justify-center gap-9">
-        <div className="flex w-full justify-center">
-          <div className="m-2 flex items-center">
-            <AppLogo className="size-6" onClick={() => {}} />
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex size-full max-h-[900px] max-w-[900px] gap-16 p-6">
+        <div className="flex grow flex-col justify-center gap-6 p-3">
+          <div className="flex w-full justify-center">
+            <div className="m-2 flex items-center">
+              <AppLogo className="size-6" onClick={() => {}} />
 
-            <div className="flex items-end font-iowan">
-              <div>Mirage</div>
+              <div className="flex items-end font-iowan">
+                <div>Mirage</div>
 
-              <span className="ml-[2px] text-xs italic">©</span>
+                <span className="ml-[2px] text-xs italic">©</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex w-full flex-col items-center">
-          <Typography variant={TypographyVariant.H2} className="font-bold">
-            Welcome Back
-          </Typography>
-
-          <Typography>Enter your email and password to</Typography>
-          <Typography>access your account</Typography>
-        </div>
-        <div className="flex size-full flex-col justify-center gap-4">
-          <div className="flex w-full flex-col gap-1">
-            <Typography variant={TypographyVariant.Span}>Server URL</Typography>
-
-            <Input
-              className="w-full"
-              icon={faLink}
-              placeholder="https://matrix-client.matrix.org"
-              constraints={[urlConstraint, nonEmptyConstraint]}
-              initialValue={serverUrl}
-              onValueChange={setServerUrl}
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <Typography variant={TypographyVariant.Span}>
-              Access token
+          <div className="flex flex-col items-center">
+            <Typography variant={TypographyVariant.H2} className="font-bold">
+              Welcome Back
             </Typography>
 
-            <Input
-              className="w-full"
-              placeholder="syt_dGhlY3Jpc3M_PAmQdRhKFWPaexp_0iK0SN"
-              initialValue={accessToken}
-              onValueChange={setAccessToken}
-              actions={[
-                {
-                  tooltip: isPasswordVisible ? "Hide token" : "Show token",
-                  icon: isPasswordVisible ? faEyeSlash : faEye,
-                  onClick: () => {
-                    setIsPasswordVisible(!isPasswordVisible)
+            <Typography>Enter your email and password to</Typography>
+            <Typography>access your account</Typography>
+          </div>
+          <div className="flex size-full flex-col justify-center gap-2">
+            <div className="flex w-full flex-col gap-1">
+              <Typography variant={TypographyVariant.Span}>
+                Server URL
+              </Typography>
+
+              <Input
+                className="w-full"
+                icon={faLink}
+                placeholder="https://matrix-client.matrix.org"
+                constraints={[urlConstraint, nonEmptyConstraint]}
+                initialValue={baseUrl}
+                onValueChange={setBaseUrl}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Typography variant={TypographyVariant.Span}>
+                Access token
+              </Typography>
+
+              <Input
+                className="w-full"
+                placeholder="syt_dGhlY3Jpc3M_PAmQdRhKFWPaexp_0iK0SN"
+                initialValue={accessToken}
+                onValueChange={setAccessToken}
+                actions={[
+                  {
+                    tooltip: isPasswordVisible ? "Hide token" : "Show token",
+                    icon: isPasswordVisible ? faEyeSlash : faEye,
+                    onClick: () => {
+                      setIsPasswordVisible(!isPasswordVisible)
+                    },
                   },
-                },
-              ]}
-            />
+                ]}
+                icon={faKey}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Typography variant={TypographyVariant.Span}>User ID</Typography>
+
+              <Input
+                initialValue={userId}
+                className="w-full"
+                icon={faUserCircle}
+                placeholder="@userId:matrix.org"
+                constraints={[userIdConstraint, nonEmptyConstraint]}
+                onValueChange={setUserId}
+              />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Button
+                label={isConnecting ? "Connecting..." : "Sign in →"}
+                isDisabled={syncState !== null && syncState !== SyncState.Error}
+                isLoading={isConnecting}
+                onClick={() => {
+                  void login()
+                }}
+              />
+
+              {/* FIXME: This is temporary. Remove later on. */}
+              <div>{lastSyncError?.message ?? "Waiting for login."}</div>
+
+              <Button
+                onClick={() => {}}
+                label="Forgot password?"
+                variant={ButtonVariant.TextLink}
+                color={ButtonColor.Black}
+              />
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <Typography variant={TypographyVariant.Span}>User ID</Typography>
 
-            <Input
-              initialValue={userId}
-              className="w-full"
-              icon={faUserCircle}
-              placeholder="@userId:matrix.org"
-              constraints={[userIdConstraint, nonEmptyConstraint]}
-              onValueChange={setUserId}
-            />
-          </div>
+          <div className="flex w-full items-center justify-center gap-1">
+            <Typography>Don&apos;t have an account?</Typography>
 
-          <div className="flex flex-col gap-1">
-            <Button
-              label={isConnecting ? "Connecting..." : "Sign in →"}
-              isDisabled={syncState !== null}
-              isLoading={isConnecting}
-              onClick={() => {
-                void login()
-              }}
-              color={ButtonColor.Black}
-            />
-
-            {/* FIXME: This is temporary. Remove later on. */}
-            <div>{lastSyncError?.message ?? "Waiting for login."}</div>
-
-            <Button
-              onClick={() => {}}
-              label="Forgot password?"
-              variant={ButtonVariant.TextLink}
-              color={ButtonColor.Black}
-            />
+            <Link to="">
+              <Typography className="font-bold">Sign up</Typography>
+            </Link>
           </div>
         </div>
 
-        <div className="flex w-full items-center justify-center gap-1">
-          <Typography>Don&apos;t have an account?</Typography>
-
-          <Link to="">
-            <Typography className="font-bold">Sign up</Typography>
-          </Link>
+        <div className="flex grow overflow-hidden rounded-xl">
+          <img className="object-fill" src="../../public/LoginPhoto.png" />
         </div>
       </div>
     </div>
