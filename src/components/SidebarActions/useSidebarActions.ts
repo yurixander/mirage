9f737fa29391b278133c3a014c95f12369245f41
criss8X <carralerocristianx@gmail.com>
2024-03-22 14:44:00 -0400

    feat: More use `useCallback` for `useSidebarActions`

--- src/components/SidebarActions/useSidebarActions.ts
+++ src/components/SidebarActions/useSidebarActions.ts
@@ -1,15 +1,16 @@
 import useConnection from "../../hooks/matrix/useConnection"
 import {useNavigate} from "react-router-dom"
 import {ViewPath} from "@/utils/util"
+import {useCallback} from "react"
 
 const useSidebarActions = () => {
   const {disconnect} = useConnection()
   const navigate = useNavigate()
 
-  const onLogout = async () => {
+  const onLogout = useCallback(async () => {
     await disconnect()
     navigate(ViewPath.Login)
-  }
+  }, [disconnect, navigate])
 
   return {onLogout}
 }
