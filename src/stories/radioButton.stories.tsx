153086b631a326d76bb622007298b40e2cfce507
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2024-03-24 16:47:17 -0400

    Update radioButton.stories.tsx
    Separando lineas

--- src/stories/radioButton.stories.tsx
+++ src/stories/radioButton.stories.tsx
@@ -6,6 +6,7 @@ type Story = StoryObj<typeof RadioButton>
 const meta: Meta<typeof RadioButton> = {
   component: RadioButton,
 }
+
 const render = (args: RadioButtonProps) => <RadioButton {...args} />
 
 export const Default: Story = {
