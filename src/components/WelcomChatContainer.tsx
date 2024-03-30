f803130a0f81478e73024de573d23511efce9441
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2024-03-30 11:16:56 -0400

    Update WelcomChatContainer.tsx

--- src/components/WelcomChatContainer.tsx
+++ src/components/WelcomChatContainer.tsx
@@ -6,8 +6,10 @@ import {StaticAssetPath} from "@/utils/util"
 const WelcomChatContainer: FC = () => {
   return (
     <div className="flex size-full flex-col items-center justify-center gap-4">
-      <ReactSVG src={StaticAssetPath.AppLogoSmall} />
+      <ReactSVG src={StaticAssetPath.AppLogo} />
+      
       <Typography variant={TypographyVariant.H1}>Welcome to Mirage</Typography>
+      
       <Typography className="text-center" variant={TypographyVariant.Span}>
         Chat with friends, family, the whole world, select a Room or a direct
         chat and start a conversation
