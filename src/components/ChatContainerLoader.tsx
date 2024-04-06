58ba977397039fc1842c953b05ee2c1515764d07
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2024-04-06 08:46:24 -0400

    fix: Restoring compatibility with prettier

--- src/components/ChatContainerLoader.tsx
+++ src/components/ChatContainerLoader.tsx
@@ -6,94 +6,72 @@ const ChatContainerLoader: FC = () => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-1">
-        
         <SkeletonCircle />
-        
+
         <div className="inline-flex flex-col gap-1">
-          
           <SkeletonLine className="h-4 w-40" />
           <SkeletonLine />
-          
         </div>
-        
       </div>
 
       <div className="flex gap-1">
-        
         <SkeletonCircle />
-        
         <div className="inline-flex flex-col gap-1">
-          
           <SkeletonLine className="h-4 w-60" />
-          
+
           <div className="flex flex-row gap-1.5">
-            
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-20" />
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-20" />
-            
           </div>
+
           <div className="flex flex-row gap-1.5">
-            
             <SkeletonLine className="h-3 w-5" />
             <SkeletonLine className="h-3 w-20" />
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-16" />
-            
           </div>
         </div>
       </div>
 
       <div className="flex gap-1">
         <SkeletonCircle />
-        
+
         <div className="inline-flex flex-col gap-1">
-          
           <SkeletonLine className="h-4 w-36" />
           <SkeletonLine className="h-52 w-44" />
-          
         </div>
-        
       </div>
 
       <div className="flex gap-1">
-        
         <SkeletonCircle />
-        
+
         <div className="inline-flex flex-col gap-1">
-          
           <SkeletonLine className="h-4 w-40" />
-          
+
           <div className="flex flex-row gap-1.5">
-            
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-20" />
             <SkeletonLine className="h-3 w-5" />
             <SkeletonLine className="h-3 w-10" />
-            
           </div>
-          
+
           <div className="flex flex-row gap-1.5">
-            
             <SkeletonLine className="h-3 w-5" />
             <SkeletonLine className="h-3 w-20" />
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-16" />
-            
           </div>
-          
+
           <div className="flex flex-row gap-1.5">
-            
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-5" />
             <SkeletonLine className="h-3 w-10" />
             <SkeletonLine className="h-3 w-5" />
             <SkeletonLine className="h-3 w-20" />
-            
           </div>
-          
         </div>
       </div>
     </div>
@@ -123,7 +101,7 @@ const SkeletonCircle: FC<SkeletonProps> = ({className = "size-10"}) => {
     <div className="relative">
       <div
         className={twMerge(
-          "relative overflow-hidden rounded-full bg-neutral-300",
+          "relative  overflow-hidden rounded-full bg-neutral-300",
           className
         )}>
         <LoadingEffect />
