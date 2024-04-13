f9ab261154c0e9c7202b4df6b01400de26a9c990
criss8X <carralerocristianx@gmail.com>
2024-04-13 15:05:40 -0400

    feat: Implement handling when `imageUrl` is `undefined`

--- src/containers/ChatContainer/ImageModal.tsx
+++ src/containers/ChatContainer/ImageModal.tsx
@@ -2,8 +2,14 @@ import IconButton from "@/components/IconButton"
 import {type FC} from "react"
 import {saveAs} from "file-saver"
 import {IoMdDownload, IoMdTrash} from "react-icons/io"
-import {IoArrowRedo, IoArrowUndo, IoCloseCircle} from "react-icons/io5"
+import {
+  IoArrowRedo,
+  IoArrowUndo,
+  IoCloseCircle,
+  IoInformationCircle,
+} from "react-icons/io5"
 import ImageZoom from "@/components/ImageZoom"
+import Typography from "@/components/Typography"
 
 export type ImageModalProps = {
   imageUrl?: string
@@ -11,14 +17,13 @@ export type ImageModalProps = {
   onDeleteImage?: () => void
 }
 
-// TODO: Handle when the imageUrl is undefined.
 const ImageModal: FC<ImageModalProps> = ({
   imageUrl,
   onDeleteImage,
   onClose,
 }) => {
   return (
-    <div className="flex size-full flex-col bg-modalOverlay">
+    <div className="flex size-full flex-col items-center justify-center  bg-modalOverlay">
       <div className="ml-auto flex gap-1 p-2">
         {onDeleteImage !== undefined && (
           <IconButton
@@ -32,36 +37,36 @@ const ImageModal: FC<ImageModalProps> = ({
           />
         )}
 
-        <IconButton
-          size={26}
-          tooltip="Resend image message"
-          Icon={IoArrowRedo}
-          onClick={function (): void {
-            throw new Error("Resend image message not implemented.")
-          }}
-        />
+        {imageUrl !== undefined && (
+          <div>
+            <IconButton
+              size={26}
+              tooltip="Resend image message"
+              Icon={IoArrowRedo}
+              onClick={function (): void {
+                throw new Error("Resend image message not implemented.")
+              }}
+            />
 
-        <IconButton
-          size={26}
-          tooltip="Reply image message"
-          Icon={IoArrowUndo}
-          onClick={function (): void {
-            throw new Error("Reply message not implemented.")
-          }}
-        />
+            <IconButton
+              size={26}
+              tooltip="Reply image message"
+              Icon={IoArrowUndo}
+              onClick={function (): void {
+                throw new Error("Reply message not implemented.")
+              }}
+            />
 
-        <IconButton
-          size={26}
-          tooltip="Download Image"
-          Icon={IoMdDownload}
-          onClick={() => {
-            if (imageUrl === undefined) {
-              return
-            }
-
-            saveAs(imageUrl)
-          }}
-        />
+            <IconButton
+              size={26}
+              tooltip="Download Image"
+              Icon={IoMdDownload}
+              onClick={() => {
+                saveAs(imageUrl)
+              }}
+            />
+          </div>
+        )}
 
         <IconButton
           size={26}
@@ -72,7 +77,15 @@ const ImageModal: FC<ImageModalProps> = ({
       </div>
 
       <div className="flex size-full items-center justify-center overflow-auto">
-        <ImageZoom src={imageUrl} />
+        {imageUrl === undefined ? (
+          <div className="flex flex-col items-center gap-2 rounded-lg bg-white p-2">
+            <IoInformationCircle size={28} color="red" />
+
+            <Typography>An error occurred while loading the image</Typography>
+          </div>
+        ) : (
+          <ImageZoom src={imageUrl} />
+        )}
       </div>
     </div>
   )
---
+++ src/stories/imageModal.stories.tsx
@@ -0,0 +1,20 @@
+import {type Meta, type StoryObj} from "@storybook/react"
+import ImageModal from "../containers/ChatContainer/ImageModal"
+
+type Story = StoryObj<typeof ImageModal>
+
+const meta: Meta<typeof ImageModal> = {
+  component: ImageModal,
+}
+const render = () => (
+  <ImageModal
+    onClose={() => {}}
+  />
+)
+
+export const Default: Story = {
+  render,
+  args: {},
+}
+
+export default meta
