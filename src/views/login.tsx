0f8c1924b666e9f12e2c1d566d4b739f8fd82050
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2024-04-15 22:40:38 -0400

    feat: Update Style
    Centrando indicaciones de inicio de sesión para el usuario

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -105,7 +105,7 @@ const LoginView: FC = () => {
               Welcome Back
             </Typography>
 
-            <Typography>
+            <Typography className="text-center">
               Enter your email and password to
               <br />
               access your account
