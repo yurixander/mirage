66b385b9d90b5642c3edb96a23ddb73ca578e4f4
criss8X <carralerocristianx@gmail.com>
2024-05-08 09:08:15 -0400

    feat: Implement `useNavigation` and `useList`

---
+++ src/hooks/matrix/useNavigation.ts
@@ -0,0 +1,74 @@
+import {useCallback, useEffect} from "react"
+import useList from "../util/useList"
+import useConnection from "./useConnection"
+import {RoomType} from "matrix-js-sdk"
+
+type Room = {
+  roomId: string
+  roomName: string
+}
+
+export type Space = {
+  name: string
+  spaceId: string
+  childRooms: Room[]
+}
+
+const hasRepeat = (space1: Space, space2: Space): boolean =>
+  space1.spaceId === space2.spaceId
+
+const useNavigation = () => {
+  const {items: spaces, addItem: addSpace} = useList<Space>(hasRepeat)
+  const {client} = useConnection()
+
+  const fetchSpaces = useCallback(() => {
+    if (client === null) {
+      return
+    }
+
+    const storeSpaces = client.getRooms().filter(room => room.isSpaceRoom())
+
+    for (const storeSpace of storeSpaces) {
+      client
+        .getRoomHierarchy(storeSpace.roomId)
+        .then(roomHierarchy => {
+          const childRooms: Room[] = []
+
+          for (const room of roomHierarchy.rooms) {
+            if (room.name === undefined || room.room_type === RoomType.Space) {
+              continue
+            }
+
+            childRooms.push({
+              roomId: room.room_id,
+              roomName: room.name,
+            })
+          }
+
+          addSpace({
+            name: storeSpace.name,
+            spaceId: storeSpace.roomId,
+            childRooms,
+          })
+        })
+        .catch(error => {
+          // TODO: Handle error here instead.
+          console.error(error)
+        })
+    }
+  }, [addSpace, client])
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      fetchSpaces()
+    }, 1000)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [fetchSpaces])
+
+  return {spaces}
+}
+
+export default useNavigation
