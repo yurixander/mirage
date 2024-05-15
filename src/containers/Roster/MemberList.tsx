d5d5011db5bf3cc253cfcd0863caf795e49fc647
criss8X <carralerocristianx@gmail.com>
2024-05-15 14:37:29 -0400

    feat: Upgrade `MemberList` component

--- src/containers/Roster/MemberList.tsx
+++ src/containers/Roster/MemberList.tsx
@@ -4,17 +4,20 @@ import Label from "@/components/Label"
 import UserProfileGhost from "@/components/UserProfileGhost"
 
 export type MemberListProps = {
+  sections: MemberSection[]
+}
+
+type MemberSection = {
   title: string
   users: RosterUserProps[]
 }
 
-const MemberList: FC<{list: MemberListProps[]}> = ({list}) => {
+const MemberList: FC<MemberListProps> = ({sections}) => {
   return (
     <div className="flex h-full max-w-56 flex-col overflow-y-scroll">
       <div className="flex flex-col gap-4">
-        {list.map(memberSection => (
+        {sections.map(memberSection => (
           <div className="flex flex-col gap-1">
-            {/* TODO: Make think as sticky header with scroll. */}
             <Label
               className="sticky top-0 z-10 ml-1 bg-white font-bold text-slate-400"
               text={`${memberSection.title} — ${memberSection.users.length}`}
