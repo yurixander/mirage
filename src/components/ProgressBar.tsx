ea68c17c8e1af4e5cd25ba93260aaf3ac9a95049
criss8X <carralerocristianx@gmail.com>
2024-06-04 17:19:33 -0400

    feat: Upgrade `ProgressBar` component

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,5 @@
 import {type FC} from "react"
 import {twMerge} from "tailwind-merge"
-import Typography, {TypographyVariant} from "./Typography"
 import {IoIosAlert, IoIosCheckmarkCircle} from "react-icons/io"
 
 export enum ProgressBarState {
@@ -9,48 +8,53 @@ export enum ProgressBarState {
   Error,
 }
 
+export enum ProgressBarVariant {
+  Linear,
+  Circular,
+}
+
 export type ProgressBarProps = {
   progress: number
-  state: ProgressBarState
+  state?: ProgressBarState
+  containsIcon?: boolean
   className?: string
 }
 
-const ProgressBar: FC<ProgressBarProps> = ({state, progress, className}) => {
+const ProgressBar: FC<ProgressBarProps> = ({
+  state,
+  progress = ProgressBarState.Progress,
+  containsIcon = true,
+  className,
+}) => {
   const isCompleted = state === ProgressBarState.Completed
   const isError = state === ProgressBarState.Error
+  const internalProgress = isCompleted || isError ? 100 : progress
 
   return (
     <div className={twMerge("flex items-center gap-1", className)}>
-      {isCompleted || isError ? (
-        <>
-          <div
-            className={twMerge(
-              "h-2 w-full rounded-full",
-              isCompleted && "bg-green-500",
-              isError && "bg-red-500"
-            )}
-          />
-
-          {isError && <IoIosAlert className="text-red-500" size={13} />}
-
-          {isCompleted && (
-            <IoIosCheckmarkCircle className="text-green-500" size={13} />
+      <div
+        className={twMerge(
+          "h-2 w-full overflow-hidden rounded-full bg-gray-300"
+        )}>
+        <div
+          className={twMerge(
+            "relative flex h-full items-center justify-center text-center text-white duration-300",
+            isCompleted
+              ? "bg-green-500"
+              : isError
+                ? "bg-red-500"
+                : "bg-purple-500"
           )}
-        </>
-      ) : (
-        <>
-          <div
-            className={twMerge(
-              "h-2 w-full overflow-hidden rounded-full bg-gray-300"
-            )}>
-            <div
-              className="relative flex h-full items-center justify-center bg-purple-500 text-center text-white duration-300"
-              style={{width: `${progress}%`}}
-            />
-          </div>
+          style={{width: `${internalProgress}%`}}
+        />
+      </div>
 
-          <Typography variant={TypographyVariant.P}>{progress}%</Typography>
-        </>
+      {isError && containsIcon && (
+        <IoIosAlert className="text-red-500" size={13} />
+      )}
+
+      {isCompleted && containsIcon && (
+        <IoIosCheckmarkCircle className="text-green-500" size={13} />
       )}
     </div>
   )
