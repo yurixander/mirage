41c8b8abe93c914d3969f35a04e7edad5edc6f3d
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-06-16 10:10:10 -0400

    feat: delete tag and otimised variant step

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,16 @@
 import React, {useState, type FC} from "react"
+import {twMerge} from "tailwind-merge"
 
 export type SliderProps = {
   min: number
   max: number
-  step?: number
   value: number
+  step?: number
+  isTooltip?: boolean
   onProgressChange: (value: number) => void
 }
 
-const Slider: FC<SliderProps> = ({onProgressChange, min, max, step, value}) => {
+const Slider: FC<SliderProps> = ({onProgressChange, min, max, value, step}) => {
   const [progress, setProgress] = useState(value)
 
   const handleOnInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,34 +21,22 @@ const Slider: FC<SliderProps> = ({onProgressChange, min, max, step, value}) => {
   }
 
   return (
-    <label className="relative flex w-60 items-center justify-center">
-      <div className="-ml-4 mb-14 flex w-60">
-        <div
-          style={{
-            left: `${progress * (240 / max) - ((progress * 100) / max / 100) * 15}px`,
-          }}
-          className="absolute flex flex-col items-center justify-center">
-          <div className="size-4 rotate-45 bg-fuchsia-600" />
-          <div className="absolute mb-4 flex h-5 w-10 items-center justify-center rounded bg-fuchsia-600 text-white">
-            {progress}%
-          </div>
-        </div>
-      </div>
-      {step === undefined || step < 10 ? (
-        <BasicProgressBar progress={(progress * 100) / (max - min)} />
-      ) : (
-        <StepProgressBar steps={(max - min) / step} />
-      )}
-
+    <label className="flex w-60 flex-col items-center justify-center">
       <input
         max={max}
         min={min}
         step={step}
         value={progress}
         onInput={handleOnInput}
-        className="absolute h-3 w-60 cursor-pointer appearance-none rounded-full bg-transparent p-0"
+        className="relative z-10 h-3 w-60 cursor-pointer appearance-none rounded-full bg-transparent p-0"
         type="range"
       />
+
+      {step === undefined || step < 10 ? (
+        <BasicProgressBar progress={(progress * 100) / (max - min)} />
+      ) : (
+        <StepProgressBar steps={(max - min) / step} />
+      )}
     </label>
   )
 }
@@ -66,22 +56,27 @@ const StepProgressBar: FC<{steps: number}> = ({steps}) => {
   return (
     <div className="absolute flex h-3 w-60 items-center overflow-hidden rounded-full bg-slate-200 shadow">
       <div className="rounded-full bg-slate-300 text-end">
-        <div className="size-2 rounded-full bg-white shadow" />
+        <div className="size-2 rounded-full bg-slate-200" />
       </div>
       {Array.from({length: steps}, (_, i) => (
-        <Step key={i} />
+        <Step isEnd={i === steps - 1} key={i} />
       ))}
     </div>
   )
 }
 
-const Step: FC = () => {
+const Step: FC<{isEnd: boolean}> = ({isEnd}) => {
   return (
     <>
       <div className="w-full" />
 
       <div>
-        <div className="size-2 rounded-full bg-white shadow" />
+        <div
+          className={twMerge(
+            "size-2 rounded-full",
+            isEnd ? "bg-slate-200" : "bg-white"
+          )}
+        />
       </div>
     </>
   )
