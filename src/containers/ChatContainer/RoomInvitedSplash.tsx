e95f8437cc7bd74921ec2f4afad5825a2cedfd6f
criss8X <carralerocristianx@gmail.com>
2024-06-24 14:37:31 -0400

    feat: Implement `RoomInvitedSplash` component

--- src/containers/ChatContainer/RoomInvitedSplash.tsx
+++ src/containers/ChatContainer/RoomInvitedSplash.tsx
@@ -1,42 +1,129 @@
 import AvatarImage, {AvatarType} from "@/components/AvatarImage"
 import Button, {ButtonVariant} from "@/components/Button"
 import Typography, {TypographyVariant} from "@/components/Typography"
-import {type FC} from "react"
+import {useMemo, useState, type FC} from "react"
 import RoomNotFoundSplash from "./RoomNotFoundSplash"
+import useConnection from "@/hooks/matrix/useConnection"
+import useActiveRoomIdStore from "@/hooks/matrix/useActiveRoomIdStore"
+import {getImageUrl} from "@/utils/util"
+
+enum ActionTypes {
+  Join,
+  Leave,
+}
 
 const RoomInvitedSplash: FC<{roomId: string | null}> = ({roomId}) => {
-  return roomId === null ? (
+  const {client} = useConnection()
+  const {setActiveRoomId, clearActiveRoomId} = useActiveRoomIdStore()
+  const [actionLoading, setActionLoading] = useState<ActionTypes | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  // Room data
+  const roomData = useMemo(() => {
+    if (client === null || roomId === null) {
+      return
+    }
+
+    const room = client.getRoom(roomId)
+
+    if (room === null) {
+      // TODO: Show toast when error happens.
+
+      return
+    }
+
+    return {
+      roomName: room.name,
+      roomAvatarUrl: getImageUrl(room.getMxcAvatarUrl(), client),
+    }
+  }, [client, roomId])
+
+  return roomId === null || roomData === undefined ? (
     <RoomNotFoundSplash />
   ) : (
     <div className="flex size-full items-center justify-center border-r border-stone-200">
-      <AvatarImage
-        isRounded
-        avatarType={AvatarType.ProfileBar}
-        displayName="Emerald Branch"
-      />
-
-      <Typography variant={TypographyVariant.Heading}>Figma Utils</Typography>
-
-      <Typography variant={TypographyVariant.Body}>
-        You have been invited to this room
-      </Typography>
-
-      <div className="flex gap-1">
-        <Button
-          label="Reject"
-          onClick={() => {
-            throw new Error("Function not implemented.")
-          }}
+      <div className="flex max-w-max flex-col items-center gap-2">
+        <AvatarImage
+          isRounded
+          avatarType={AvatarType.ProfileBar}
+          displayName={roomData.roomName}
+          avatarUrl={roomData.roomAvatarUrl}
         />
 
-        <Button
-          variant={ButtonVariant.Primary}
-          label="Accept"
-          onClick={() => {
-            throw new Error("Function not implemented.")
-          }}
-        />
+        <Typography variant={TypographyVariant.Heading}>
+          {roomData.roomName}
+        </Typography>
+
+        <Typography variant={TypographyVariant.Body}>
+          You have been invited to this room
+        </Typography>
+
+        <div className="mt-1 flex w-full gap-1">
+          <Button
+            className="w-full"
+            label="Reject"
+            isDisabled={client === null || actionLoading === ActionTypes.Join}
+            isLoading={actionLoading === ActionTypes.Leave}
+            onClick={() => {
+              if (client === null) {
+                return
+              }
+
+              setError(null)
+              setActionLoading(ActionTypes.Leave)
+
+              void client
+                .leave(roomId)
+                .then(() => {
+                  setActionLoading(null)
+                  clearActiveRoomId()
+                })
+                .catch(_error => {
+                  // TODO: Show toast when error happens.
+
+                  setError(
+                    "An error occurred while trying to reject the invitation."
+                  )
+
+                  setActionLoading(null)
+                })
+            }}
+          />
+
+          <Button
+            className="w-full"
+            isDisabled={client === null || actionLoading === ActionTypes.Leave}
+            isLoading={actionLoading === ActionTypes.Join}
+            variant={ButtonVariant.Primary}
+            label="Accept"
+            onClick={() => {
+              if (client === null) {
+                return
+              }
+
+              setError(null)
+              setActionLoading(ActionTypes.Join)
+
+              void client
+                .joinRoom(roomId)
+                .then(room => {
+                  setActionLoading(null)
+
+                  clearActiveRoomId()
+                  setActiveRoomId(room.roomId)
+                })
+                .catch(_error => {
+                  // TODO: Show toast when error happens.
+
+                  setError("An error occurred while trying to join the Room.")
+                  setActionLoading(null)
+                })
+            }}
+          />
+        </div>
       </div>
+
+      {error !== null && <Typography className="mt-4">{error}</Typography>}
     </div>
   )
 }
