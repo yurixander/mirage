e7ef5b4ad038eed8c086c76a3770f77fd128cf60
Cristian Carralero <carralerocristianX@gmail.com>
2024-07-26 21:28:53 -0400

    feat: Implement ´AudioMessage´ component

--- src/components/AudioMessage.tsx
+++ src/components/AudioMessage.tsx
@@ -1,11 +1,21 @@
-import {type FC} from "react"
-import {IoPlay} from "react-icons/io5"
+import {useEffect, useRef, useState, type FC} from "react"
+import {IoPause, IoPlay, IoReload, IoWarning} from "react-icons/io5"
 import Typography, {TypographyVariant} from "./Typography"
 import AvatarImage, {AvatarType} from "./AvatarImage"
 import {type ContextMenuItem} from "./ContextMenu"
 import {formatTime} from "@/utils/util"
+import IconButton from "./IconButton"
+
+enum AudioState {
+  Ready,
+  Loading,
+  Error,
+}
 
 export type AudioMessageProps = {
+  audioUrl: string
+  setCurrentPlaying: (value: boolean) => void
+  isCurrentPlaying: boolean
   authorDisplayName: string
   timestamp: number
   id: string
@@ -15,35 +25,91 @@ export type AudioMessageProps = {
 }
 
 const AudioMessage: FC<AudioMessageProps> = ({
+  audioUrl,
+  isCurrentPlaying,
+  setCurrentPlaying,
   contextMenuItems,
-  id,
   onAuthorClick,
   timestamp,
   authorAvatarUrl,
   authorDisplayName,
 }) => {
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const [audioState, setAudioState] = useState(AudioState.Loading)
+  const isError = audioState === AudioState.Error
+  const isLoading = audioState === AudioState.Loading
+
+  useEffect(() => {
+    if (isCurrentPlaying || audioRef.current === null) {
+      return
+    }
+
+    audioRef.current.pause()
+  }, [isCurrentPlaying])
+
   return (
-    <div className="flex size-full max-h-14 max-w-60 items-center rounded-xl border-2 border-gray-100 bg-white p-2 shadow-sm">
-      <IoPlay
-        className="shrink-0 cursor-pointer text-purple-500"
-        onClick={() => {
-          // TODO: Handle audio playback here.
+    <>
+      <div className="flex size-full max-h-14 max-w-60 items-center rounded-xl border-2 border-gray-100 bg-white p-2 shadow-sm">
+        <IconButton
+          tooltip="Playback"
+          // TODO: Optimize icons change detection.
+          // TODO: Implement better icon for loading for more performance.
+          Icon={
+            isError
+              ? IoWarning
+              : isLoading
+                ? IoReload
+                : isCurrentPlaying
+                  ? IoPause
+                  : IoPlay
+          }
+          onClick={() => {
+            if (isCurrentPlaying) {
+              audioRef.current?.pause()
+              setCurrentPlaying(false)
+            } else {
+              void audioRef.current?.play()
+              setCurrentPlaying(true)
+            }
+          }}
+        />
+
+        <div className="ml-auto flex items-center gap-2">
+          <Typography>{formatTime(timestamp)}</Typography>
+
+          <AvatarImage
+            isRounded
+            avatarType={AvatarType.Profile}
+            displayName={authorDisplayName}
+            avatarUrl={authorAvatarUrl}
+          />
+        </div>
+      </div>
+
+      <audio
+        ref={audioRef}
+        preload="auto"
+        src={audioUrl}
+        onLoadStart={() => {
+          setAudioState(AudioState.Loading)
+        }}
+        onCanPlayThrough={() => {
+          setAudioState(AudioState.Ready)
+        }}
+        onError={() => {
+          setAudioState(AudioState.Error)
+        }}
+        onEnded={() => {
+          if (audioRef.current === null) {
+            return
+          }
+
+          audioRef.current.currentTime = 0
+
+          setAudioState(AudioState.Ready)
         }}
       />
-
-      <div className="ml-auto flex items-center gap-2">
-        <Typography variant={TypographyVariant.Heading}>
-          {formatTime(timestamp)}
-        </Typography>
-
-        <AvatarImage
-          isRounded
-          avatarType={AvatarType.Profile}
-          displayName={authorDisplayName}
-          avatarUrl={authorAvatarUrl}
-        />
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,7 +1,50 @@
-import {type FC} from "react"
+import AudioMessage from "@/components/AudioMessage"
+import {useState, type FC} from "react"
 
 const DevelopmentPreview: FC = () => {
-  return <></>
+  const [audioPlaying, setAudioPlaying] = useState<string>()
+
+  return (
+    <div className="flex flex-col gap-4">
+      <AudioMessage
+        id="audio-1"
+        audioUrl="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+        isCurrentPlaying={"audio-1" === audioPlaying}
+        authorDisplayName="John Doe"
+        timestamp={Date.now()}
+        contextMenuItems={[]}
+        onAuthorClick={function (): void {
+          throw new Error("Function not implemented.")
+        }}
+        setCurrentPlaying={(value: boolean) => {
+          if (value) {
+            setAudioPlaying("audio-1")
+          } else {
+            setAudioPlaying(undefined)
+          }
+        }}
+      />
+
+      <AudioMessage
+        id="audio-2"
+        audioUrl="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+        isCurrentPlaying={"audio-2" === audioPlaying}
+        authorDisplayName="John Doe"
+        timestamp={Date.now()}
+        contextMenuItems={[]}
+        onAuthorClick={function (): void {
+          throw new Error("Function not implemented.")
+        }}
+        setCurrentPlaying={(value: boolean) => {
+          if (value) {
+            setAudioPlaying("audio-2")
+          } else {
+            setAudioPlaying(undefined)
+          }
+        }}
+      />
+    </div>
+  )
 }
 
 export default DevelopmentPreview
