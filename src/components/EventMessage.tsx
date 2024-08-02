05a1a80e0a908393e31770bc4aff8af2e62a0ce0
Cristian Carralero <carralerocristianX@gmail.com>
2024-08-02 02:08:26 -0400

    feat: Implement new handling for ´EventMessage´ (#66)

--- src/components/EventMessage.tsx
+++ src/components/EventMessage.tsx
@@ -1,27 +1,73 @@
 import {type FC} from "react"
-import {formatTime} from "../utils/util"
+import {formatTime, stringToColor} from "../utils/util"
 import {IoMdCreate} from "react-icons/io"
+import Typography from "./Typography"
+import ContextMenu, {ClickActions} from "./ContextMenu"
+import {IoPeopleCircle, IoSearchCircle} from "react-icons/io5"
+import {type IconType} from "react-icons"
 
-export type EventMessageProps = {
-  timestamp: number
-  text: string
-  id: string
+export type EventSender = {
+  displayName: string
+  userId: string
 }
 
-const EventMessage: FC<EventMessageProps> = ({timestamp, text}) => {
-  const localeTimeString = formatTime(timestamp)
+export type EventMessageData = {
+  eventId: string
+  sender: EventSender
+  body: string
+  timestamp: number
+  icon?: IconType
+}
+
+export interface EventMessageProps extends EventMessageData {
+  onShowMember: () => void
+  onFindUser: () => void
+}
+
+const EventMessage: FC<EventMessageProps> = ({
+  timestamp,
+  body,
+  sender,
+  eventId,
+  icon,
+  onFindUser,
+  onShowMember,
+}) => {
+  const Icon = icon ?? IoMdCreate
 
   return (
     <div className="flex items-center gap-3">
       <div className="flex w-10 items-center justify-end">
-        <IoMdCreate className="text-neutral-200" />
+        <Icon className="text-neutral-300" />
       </div>
 
-      <div className="max-w-messageMaxWidth select-text whitespace-pre-line break-words italic leading-160">
-        {text}
-      </div>
+      <Typography className="inline-flex max-w-text select-text gap-1 whitespace-pre-line break-words italic">
+        <ContextMenu
+          className="shrink-0"
+          id={`context-menu-event-${eventId}`}
+          actionType={ClickActions.LeftClick}
+          elements={[
+            {
+              icon: IoPeopleCircle,
+              onClick: onShowMember,
+              text: "View member",
+            },
+            {
+              icon: IoSearchCircle,
+              onClick: onFindUser,
+              text: "Find user",
+            },
+          ]}>
+          <Typography
+            className="font-bold"
+            style={{color: stringToColor(sender.displayName)}}>
+            {sender.displayName}
+          </Typography>
+        </ContextMenu>
+        {body}
+      </Typography>
 
-      <time className="ml-auto text-gray-300">{localeTimeString}</time>
+      <time className="ml-auto text-gray-300">{formatTime(timestamp)}</time>
     </div>
   )
 }
