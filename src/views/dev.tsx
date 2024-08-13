19f11ca419e48ed9af4d0be1add05321c8ca7fb5
Cristian Carralero <carralerocristianX@gmail.com>
2024-08-13 18:17:03 -0400

    feat: Test `tippy` for tooltips

--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,7 +1,25 @@
 import {type FC} from "react"
+import {IoIceCream} from "react-icons/io5"
+import Tippy from "@tippyjs/react"
+import "tippy.js/dist/tippy.css"
+import "tippy.js/animations/scale-subtle.css"
 
 const DevelopmentPreview: FC = () => {
-  return <></>
+  return (
+    <>
+      <Tippy
+        content="Ice cream"
+        arrow
+        inertia
+        animation="scale-subtle"
+        duration={100}
+        placement="top">
+        <button>
+          <IoIceCream size={36} />
+        </button>
+      </Tippy>
+    </>
+  )
 }
 
 export default DevelopmentPreview
