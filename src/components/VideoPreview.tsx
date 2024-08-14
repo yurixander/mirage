15d2517f6b99c7714b558dfa340b5263806bdcc6
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-08-13 22:20:04 -0400

    feat: Implement thumbnail

--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -1,50 +1,57 @@
 import {IoAdd, IoCloseCircle} from "react-icons/io5"
 import Button, {ButtonVariant} from "./Button"
-import IconButton from "./IconButton"
 import Typography, {TypographyVariant} from "./Typography"
 import {useRef, useState, type FC} from "react"
 
 const VideoPreview: FC = () => {
+  const [url, setUrl] = useState("")
+
+  const handleFileSelected = (e: {
+    target: {files: Array<Blob | MediaSource>}
+  }): void => {
+    const fileURL = URL.createObjectURL(e.target.files[0])
+    setUrl(fileURL)
+  }
+
   const videoRef = useRef(null)
-  const canvasRef = useRef(null)
   const imageRef = useRef(null)
+
   return (
     <div className="flex size-messageMaxWidth flex-col rounded border bg-slate-50 shadow-xl">
-      <div className="flex w-full rounded-t border-b-2 border-b-slate-200 bg-slate-100 p-5">
+      <div className="flex w-full items-start rounded-t border-b-2 border-b-slate-200 bg-slate-100 p-5">
         <Typography className="w-full" variant={TypographyVariant.Heading}>
           Video
         </Typography>
-        <IconButton tooltip="Exit" onClick={() => {}} Icon={IoCloseCircle} />
+        <IoCloseCircle size={20} />
       </div>
       <div className="flex h-full flex-col">
         <div className="flex size-full items-center justify-center p-2">
           <video
-            className="w-full rounded bg-green-500"
-            src="http://localhost/apps/imagenes/vid.mp4"
+            className="size-full max-h-60 rounded bg-black"
+            src={url}
             controls
             ref={videoRef}
+            onLoadedData={e => {
+              getThumbnail(videoRef.current, imageRef.current)
+            }}
           />
         </div>
         <div className="flex items-center justify-center gap-2 p-2">
-          <button
-            onClick={() => {
-              getThumbnail(
-                videoRef.current,
-                canvasRef.current,
-                imageRef.current
-              )
-            }}
-            className="flex size-12 items-center justify-center rounded border-2 border-slate-400 bg-slate-200 p-1">
-            <canvas className="size-12" ref={canvasRef}></canvas>
+          <div className="flex size-14 items-center justify-center rounded bg-slate-100 shadow hover:bg-slate-300">
             <img
               alt="frame"
-              className="size-full rounded shadow"
+              className="size-full rounded bg-black object-contain shadow"
               ref={imageRef}
             />
-          </button>
-          <button className="flex size-12 items-center justify-center rounded border-2 border-slate-400 bg-slate-200">
-            <IoAdd size={40} className="text-slate-400" />
-          </button>
+          </div>
+
+          <IoAdd
+            role="button"
+            className="flex size-14 items-center justify-center rounded border-2 border-slate-300 bg-slate-100 text-slate-300 shadow"
+            onClick={() => {
+              pikerVideo(handleFileSelected)
+            }}
+          />
         </div>
       </div>
       <div className="flex justify-end gap-3 rounded-b border-t-2 border-t-slate-200 bg-slate-100 p-3">
@@ -67,12 +74,8 @@ const VideoPreview: FC = () => {
 
 const getThumbnail = (
   video: HTMLVideoElement | null,
-  canvas: HTMLCanvasElement | null,
   image: HTMLImageElement | null
 ): void => {
-  if (canvas === null) {
-    return
-  }
   if (video === null) {
     return
   }
@@ -80,10 +83,28 @@ const getThumbnail = (
     return
   }
 
-  const context = canvas.getContext("2d")
-  context?.drawImage(video, 0, 0, canvas.width, canvas.height)
-  image.src = canvas.toDataURL()
-  canvas.style.display = "none"
+  const virtualVideo = document.createElement("video")
+  virtualVideo.src = video.src
+  const obtain = (): void => {
+    const canvas = document.createElement("canvas")
+    const context = canvas.getContext("2d")
+    context?.drawImage(virtualVideo, 0, 0, canvas.width, canvas.height)
+    image.src = canvas.toDataURL()
+  }
+
+  virtualVideo.currentTime = 1
+  virtualVideo.addEventListener("seeked", obtain)
+  virtualVideo.remove()
+}
+
+const pikerVideo = (
+  handleFileSelected: (e: {target: {files: Array<Blob | MediaSource>}}) => void
+): void => {
+  const inputFile = document.createElement("input")
+  inputFile.type = "file"
+  inputFile.accept = "video/*"
+  inputFile.addEventListener("change", handleFileSelected)
+  inputFile.click()
 }
 
 export default VideoPreview
