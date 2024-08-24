04aee6ae96f733b7812fdeb7c365fa708893cd82
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-08-24 15:08:21 -0400

    fix: remove comment

--- src/containers/RoomContainer/ImageModalPreview.tsx
+++ src/containers/RoomContainer/ImageModalPreview.tsx
@@ -26,29 +26,6 @@ const ImageModalPreview: FC<ImageModalPreviewProps> = ({
             onSendImage={onSendImage}
             onClear={onClear}
           />
-          {/* <div className="flex max-h-[600px] max-w-xl flex-col gap-4 rounded-xl bg-slate-50 p-6 px-8 shadow-md">
-            <img
-              className="h-auto w-full rounded-lg object-cover shadow-md"
-              src={imageUrl}
-              alt={imageName}
-            />
-            <div className="flex w-full items-center justify-end gap-1">
-              <Button
-                variant={ButtonVariant.Secondary}
-                onClick={onClear}
-                label="Cancel"
-              />
-
-              <Button
-                variant={ButtonVariant.Primary}
-                label="Send Image"
-                onClick={() => {
-                  onSendImage()
-                  onClear()
-                }}
-              />
-            </div>
-          </div> */}
         </div>,
         document.body
       )}
