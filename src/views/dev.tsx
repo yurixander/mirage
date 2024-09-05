b3930f55e503716a5524b5d2e992ed2d8cb46ef4
Cristian Carralero <carralerocristianX@gmail.com>
2024-09-05 15:56:19 -0400

    feat: Implement `ScrollArea` from shadcn

--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,9 +1,17 @@
 import {type FC} from "react"
+import {ScrollArea} from "@/components/ui/scroll-area"
 
 const DevelopmentPreview: FC = () => {
   return (
     <>
-      <div></div>
+      <ScrollArea type="scroll" className="h-full w-[350px] p-4">
+        Jokester began sneaking into the castle in the middle of the night and
+        leaving jokes all over the place: under the king's pillow, in his soup,
+        even in the royal toilet. The king was furious, but he couldn't seem to
+        stop Jokester. And then, one day, the people of the kingdom discovered
+        that the jokes left by Jokester were so funny that they couldn't help
+        but laugh. And once they started laughing, they couldn't stop.
+      </ScrollArea>
     </>
   )
 }
