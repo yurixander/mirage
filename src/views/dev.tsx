1e4b1845955a843c4b9aad2730580da10d383639
Cristian Carralero <carralerocristianX@gmail.com>
2024-09-11 12:30:54 -0400

    feat: Basic handling for RecordAudio

--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,7 +1,57 @@
-import {type FC} from "react"
+import {Button} from "@/components/ui/button"
+import {useWavesurfer} from "@wavesurfer/react"
+import {useEffect, useRef, useState, type FC} from "react"
+import RecordPlugin from "wavesurfer.js/dist/plugins/record"
 
 const DevelopmentPreview: FC = () => {
-  return <></>
+  const waveformRef = useRef(null)
+  const buttonRecord = useRef<HTMLButtonElement>(null)
+  const [isRecording, setIsRecording] = useState(false)
+
+  const {wavesurfer, isReady} = useWavesurfer({
+    container: waveformRef,
+    waveColor: "#ddd",
+    progressColor: "#4a90e2",
+    cursorColor: "#4a90e2",
+    barWidth: 3,
+    barGap: 4,
+    barRadius: 20,
+    cursorWidth: 0,
+    height: 40,
+  })
+
+  useEffect(() => {
+    if (wavesurfer === null || buttonRecord.current === null) {
+      return
+    }
+
+    const recordPlugin = wavesurfer.registerPlugin(RecordPlugin.create())
+
+    buttonRecord.current.addEventListener("click", () => {
+      if (recordPlugin.isRecording()) {
+        recordPlugin.stopRecording()
+        setIsRecording(false)
+      } else {
+        void RecordPlugin.getAvailableAudioDevices().then(devices => {
+          void recordPlugin.startRecording(devices[1])
+        })
+
+        setIsRecording(true)
+      }
+    })
+  }, [wavesurfer])
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div ref={waveformRef} className="max-h-12 w-full" />
+
+        <Button ref={buttonRecord} className="w-max">
+          {isRecording ? "Stop" : "Record"}
+        </Button>
+      </div>
+    </>
+  )
 }
 
 export default DevelopmentPreview
