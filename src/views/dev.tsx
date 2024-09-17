9df827c2409b1651f04c533c3f969696580d4679
Cristian Carralero <carralerocristianX@gmail.com>
2024-09-17 18:51:22 -0400

    feat: Clean code

--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,32 +1,7 @@
-import {Button} from "@/components/ui/button"
-import AudioRecorder, {
-  AudioRecorderState,
-} from "@/containers/RoomContainer/AudioRecorder"
-import {useState, type FC} from "react"
+import {type FC} from "react"
 
 const DevelopmentPreview: FC = () => {
-  const [state, setState] = useState(AudioRecorderState.Idle)
-
-  return (
-    <>
-      <Button
-        onClick={() => {
-          setState(AudioRecorderState.Recording)
-        }}>
-        Record
-      </Button>
-
-      {state !== AudioRecorderState.Idle && (
-        <AudioRecorder
-          recorderState={state}
-          onSendAudioMessage={async function (content: Blob): Promise<void> {
-            throw new Error("Function not implemented.")
-          }}
-          onStateChange={setState}
-        />
-      )}
-    </>
-  )
+  return <></>
 }
 
 export default DevelopmentPreview
