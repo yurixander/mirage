75893e53e5467ff30a96a09166becbf39b8588f8
Cristian Carralero <carralerocristianX@gmail.com>
2024-09-28 12:37:43 -0400

    feat: Implement `SearchInput` and dark mode for input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 import {cn} from "@/utils/utils"
+import {IoSearch} from "react-icons/io5"
+import useDebounced from "@/hooks/util/useDebounced"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -11,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-7 w-full rounded border border-input bg-gray-50 px-2.5 py-0.5 text-sm text-slate-500 shadow-sm outline-1 outline-slate-300 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-slate-400 focus-visible:outline-1 focus-visible:outline-slate-400 disabled:cursor-not-allowed disabled:opacity-50 sm:h-9 sm:rounded-sm sm:px-3 sm:py-1 sm:text-base sm:outline-2 sm:focus-visible:outline-2",
+          "flex h-7 w-full rounded border border-input bg-gray-50 px-2.5 py-0.5 text-sm text-slate-500 shadow-sm outline-1 outline-offset-0 outline-slate-300 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-slate-400 focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-slate-400 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-neutral-800 dark:text-neutral-400 dark:outline-neutral-700 dark:placeholder:text-neutral-500 sm:h-9 sm:rounded-sm sm:px-3 sm:py-1 sm:text-base sm:outline-2 sm:focus-visible:outline-2",
           className
         )}
         ref={ref}
@@ -35,7 +37,7 @@ const InputIcon: React.FC<InputIconProps> = ({
   <div
     {...props}
     className={cn(
-      "pointer-events-none absolute inset-x-0 left-1 h-auto w-max text-gray-300 sm:left-1.5",
+      "pointer-events-none absolute inset-x-0 left-1 h-auto w-max text-gray-300 dark:text-neutral-500 sm:left-1.5",
       className
     )}>
     {children}
@@ -60,4 +62,61 @@ const InputRoot: React.FC<InputRootProps> = ({
 
 InputRoot.displayName = "InputRoot"
 
-export {Input, InputIcon, InputRoot}
+export type SearchInputProps = {
+  initialValue?: string
+  hasIcon?: boolean
+  searchDelay?: number
+  className?: string
+  placeholder?: string
+  onQueryDebounceChange: (query: string) => void
+}
+
+const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
+  (
+    {
+      hasIcon = true,
+      searchDelay = 500,
+      placeholder = "Search...",
+      onQueryDebounceChange,
+      className,
+      initialValue,
+    },
+    ref
+  ) => {
+    const [query, setQuery] = React.useState(initialValue ?? "")
+    const debouncedQuery = useDebounced(query, searchDelay)
+
+    React.useEffect(() => {
+      onQueryDebounceChange(debouncedQuery)
+    }, [debouncedQuery, onQueryDebounceChange])
+
+    const InputElement = (
+      <Input
+        type="search"
+        ref={ref}
+        className={cn("pl-7 sm:pl-9", className)}
+        placeholder={placeholder}
+        value={query}
+        onChange={event => {
+          setQuery(event.target.value)
+        }}
+      />
+    )
+
+    return hasIcon ? (
+      <InputRoot>
+        <InputIcon>
+          <IoSearch className="size-4 sm:size-5" />
+        </InputIcon>
+
+        {InputElement}
+      </InputRoot>
+    ) : (
+      InputElement
+    )
+  }
+)
+
+SearchInput.displayName = "SearchInput"
+
+export {Input, InputIcon, InputRoot, SearchInput}
--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,22 +1,13 @@
 import EmojiPicker from "@/components/EmojiPicker"
+import {Input} from "@/components/ui/input"
 import {type FC} from "react"
-import {InputRoot, Input, InputIcon} from "@/components/ui/input"
-import {IoSearch} from "react-icons/io5"
 
 const DevelopmentPreview: FC = () => {
   return (
     <>
       <div className="flex size-full items-center justify-center">
         {/* <EmojiPicker onPickEmoji={emoji => {}} /> */}
-        <div className="grid w-full max-w-sm items-center gap-1.5 p-1">
-          <InputRoot>
-            <InputIcon>
-              <IoSearch className="size-4 text-gray-300 sm:size-5" />
-            </InputIcon>
-
-            <Input className="pl-7 sm:pl-9" placeholder="Buscar..." />
-          </InputRoot>
-        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5 p-1"></div>
       </div>
     </>
   )
