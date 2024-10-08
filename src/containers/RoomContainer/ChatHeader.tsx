3a6b224376e3e33d404cbc2f70f80441498b9fcc
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-10-08 07:00:52 -0400

    fix: Prettier Problems

--- src/containers/RoomContainer/ChatHeader.tsx
+++ src/containers/RoomContainer/ChatHeader.tsx
@@ -35,8 +35,8 @@ const ChatHeader: FC<ChatHeaderProps> = ({
   const {t} = useTranslation()
 
   return (
-    <header className={twMerge(className, "flex flex-col gap-1 w-full")}>
-      <div className="flex items-center justify-center w-full">
+    <header className={twMerge(className, "flex w-full flex-col gap-1")}>
+      <div className="flex w-full items-center justify-center">
         <div className="m-2 flex w-full gap-1">
           <div>
             <LiaSlackHash className="text-blue-800" />
