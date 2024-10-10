d0aff6040f7ba89fd650a971401bf1cbdf2c455f
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-10-10 18:57:58 -0400

    feat: Implement dark mode

--- src/components/RoomEncryptedIndicator.tsx
+++ src/components/RoomEncryptedIndicator.tsx
@@ -9,12 +9,14 @@ const RoomEncryptedIndicator: FC = () => {
 
   return (
     <div className="z-20 flex w-full items-center justify-center p-2">
-      <div className="flex animate-pulse items-center justify-center gap-1 rounded-xl border bg-white px-10">
+      <div className="flex animate-pulse items-center justify-center gap-1 rounded-xl border bg-slate-50 px-10 dark:border-slate-500 dark:bg-slate-900">
         <div>
-          <IoLockClosed className="text-slate-500" />
+          <IoLockClosed className="text-slate-500 dark:text-slate-300" />
         </div>
 
-        <Typography>{t(LangKey.RoomEncrypted)}</Typography>
+        <Typography className="dark:text-slate-300">
+          {t(LangKey.RoomEncrypted)}
+        </Typography>
       </div>
     </div>
   )
