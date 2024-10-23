4305ff9f4d9217f3427b9209b168a2ac933cb6dd
Cristian Carralero <carralerocristianX@gmail.com>
2024-10-22 23:21:06 -0400

    feat: Implement mobile version for `EventMessage`

--- src/views/dev.tsx
+++ src/views/dev.tsx
@@ -1,7 +1,12 @@
+import EventMessage from "@/components/EventMessage"
 import {type FC} from "react"
 
 const DevelopmentPreview: FC = () => {
-  return <></>
+  return (
+    <>
+      <div className="m-1"></div>
+    </>
+  )
 }
 
 export default DevelopmentPreview
