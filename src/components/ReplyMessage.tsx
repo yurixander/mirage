96a89d90d115a0d07ba8298814a77e40444b9cc7
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2024-10-27 16:34:17 -0400

    feat: implement dark mode in reply message (#203)
    * feat: Implement dark mode
    
    * fix: Resolving pending TODO
    
    Adding tooltip with error message in case the ID of the message being answered is not resolved
    
    * feat: Implement tooltip
    
    * feat: Solving Change Requests
    
    - Modificando mensaje en el tooltip
    - Refactorización general

--- src/components/ReplyMessage.tsx
+++ src/components/ReplyMessage.tsx
@@ -1,9 +1,16 @@
 import {type FC} from "react"
 import AvatarImage, {AvatarSize, AvatarType} from "./AvatarImage"
 import TextMessage from "./TextMessage"
-import Typography, {TypographyVariant} from "./Typography"
 import {type MessageBaseData, type MessageBaseProps} from "./MessageContainer"
 import {cleanDisplayName, stringToColor} from "@/utils/util"
+import {Text} from "./ui/typography"
+import useTooltip from "@/hooks/util/useTooltip"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export interface ReplyMessageProps extends MessageBaseProps {
   onQuoteMessageClick: (quoteMessageId: string) => void
@@ -38,41 +45,58 @@ const ReplyMessage: FC<ReplyMessageProps> = ({
   onQuoteMessageClick,
   userId,
 }) => {
+  const {renderRef, showTooltip} = useTooltip<HTMLButtonElement>()
+
+  const handleTooltip = (): void => {
+    if (quotedMessageId === undefined) {
+      showTooltip("Message not found", true)
+
+      return
+    }
+    onQuoteMessageClick(quotedMessageId)
+  }
+
+  const quoteMessage = (
+    <button
+      type="button"
+      ref={renderRef}
+      onClick={handleTooltip}
+      className="-mt-1 flex items-center gap-1 overflow-hidden rounded-full border bg-gray-50 p-2 px-3 text-left hover:bg-gray-100 dark:bg-neutral-950 dark:hover:bg-neutral-900">
+      <AvatarImage
+        avatarType={AvatarType.Message}
+        displayName={quotedUserDisplayName}
+        isRounded={false}
+        avatarSize={AvatarSize.ExtraSmall}
+        avatarUrl={quotedAvatarUrl}
+      />
+
+      <Text
+        weight="bold"
+        size="1"
+        className="w-max shrink-0 select-text"
+        style={{color: stringToColor(quotedUserDisplayName)}}>
+        {cleanDisplayName(quotedUserDisplayName)}
+      </Text>
+
+      <Text size="1" className="line-clamp-1 max-w-40 shrink-0">
+        {quotedText}
+      </Text>
+    </button>
+  )
+
   return (
     <div className="flex flex-col">
       <div className="flex w-messageMaxWidth items-end">
-        <div className="ml-5 h-4 w-8 rounded-tl border-l-2 border-t-2 border-slate-200" />
+        <div className="ml-5 h-4 w-8 rounded-tl border-l-2 border-t-2 border-slate-200 dark:border-slate-800" />
+        <TooltipProvider delayDuration={500}>
+          <Tooltip>
+            <TooltipTrigger tabIndex={-1} asChild>
+              {quoteMessage}
+            </TooltipTrigger>
 
-        <button
-          onClick={() => {
-            if (quotedMessageId === undefined) {
-              // TODO: toast quotedMessageId is undefined or error
-              return
-            }
-            onQuoteMessageClick(quotedMessageId)
-          }}
-          className="-mt-1 flex items-center gap-1 overflow-hidden rounded-full border bg-gray-50 p-2 px-3 text-left hover:bg-gray-100">
-          <AvatarImage
-            avatarType={AvatarType.Message}
-            displayName={quotedUserDisplayName}
-            isRounded={false}
-            avatarSize={AvatarSize.ExtraSmall}
-            avatarUrl={quotedAvatarUrl}
-          />
-
-          <Typography
-            className="w-max shrink-0 select-text font-bold"
-            style={{color: stringToColor(quotedUserDisplayName)}}
-            variant={TypographyVariant.BodySmall}>
-            {cleanDisplayName(quotedUserDisplayName)}
-          </Typography>
-
-          <Typography
-            className="line-clamp-1 max-w-40 shrink-0"
-            variant={TypographyVariant.BodySmall}>
-            {quotedText}
-          </Typography>
-        </button>
+            <TooltipContent>{quotedText}</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
 
       <TextMessage
